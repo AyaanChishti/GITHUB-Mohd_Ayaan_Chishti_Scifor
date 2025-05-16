6e74b17436296612b2fd450b91cda46720add9b4
AyaanChishti <101421283+AyaanChishti@users.noreply.github.com>
2025-05-16 19:21:13 +0530

    Add files via upload

---
+++ Tests/Test6/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Welcome from './Welcome';
+import HelloComponent from './HelloComponent';
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Ayaan" />
+      <HelloComponent />
+    </div>
+  );
+}
+
+export default App;
